19a47fe70076f501f3cb5b0276a4ea7ea48de383
synergy2411 <synergy2411@outlook.com>
2018-07-25 11:35:38 +0530

    built-in directives added

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,25 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 console.log("APP LOADED SUCCESFULLY!");
+(function(){
+    var app = angular.module("UserModule", []);
+    app.controller("UserController", function($scope){
+        $scope.helloMsg = "Hello World!";
+        $scope.user = user;
+        $scope.moreInfo = function(user){
+            alert(user.firstName + " is working with ");
+        }
+    })
+})();
+
+// Model
+user = {
+    firstName : "Bill",
+    lastName : "Gates",
+    dob : new Date("Dec 21, 1965"),
+    income : 50000,
+    isWorking : true,
+    company : "Microsoft",
+    image : "./assets/images/users_images/bill.jpg",
+    vote : 120
+}
 },{}]},{},[1])
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
 console.log("APP LOADED SUCCESFULLY!");
+(function(){
+    var app = angular.module("UserModule", []);
+    app.controller("UserController", function($scope){
+        $scope.helloMsg = "Hello World!";
+        $scope.user = user;
+        $scope.moreInfo = function(user){
+            alert(user.firstName + " is working with " + user.company + "!");
+        }
+    })
+})();
+
+// Model
+user = {
+    firstName : "Bill",
+    lastName : "Gates",
+    dob : new Date("Dec 21, 1965"),
+    income : 50000,
+    isWorking : true,
+    company : "Microsoft",
+    image : "./assets/images/users_images/bill.jpg",
+    vote : 120
+}
